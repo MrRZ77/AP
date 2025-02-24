31a2ee8b9ccf0968db740ad9f2e8b9fb42dae634
HelloWorld <business.reza.7757@gmail.com>
2025-02-24 15:00:12 +0330

    mgh calculator practice & testing out the methods & syntax.

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hello World");
+        final double AC_GRAVITY = 9.81;
+        System.out.print("Welcome to gravitational potential calculator please enter the mass of the object in kilograms : ");
+        Scanner scanner = new Scanner(System.in);
+        double mass = scanner.nextDouble();
+        System.out.print("Please enter the height of the object in meters : ");
+        double height = scanner.nextDouble();
+        double mgh = mass * height * AC_GRAVITY;
+        System.out.println("The gravitational potential energy is : " + mgh + " joules");
     }
 }
