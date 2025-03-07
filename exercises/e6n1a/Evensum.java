f80ffa0e7be09866d75a49826fc7097473d409f8
HelloWorld <business.reza.7757@gmail.com>
2025-03-07 14:55:04 +0330

    Exercise chapter6 number1 part a second method

--- exercises/e6n1a/Evensum.java
+++ exercises/e6n1a/Evensum.java
@@ -2,12 +2,21 @@ package exercises.e6n1a;
 
 public class Evensum {
     public static void main(String[] args) {
+        //question asked for between 2 and 100 not including them.
         int startingNumber = 2;
         int endingNumber = 100;
         int sum = 0;
-        for (int i = startingNumber; i < endingNumber; i = i + 2) {
-            sum += i;
+        for (int i = startingNumber; i < endingNumber; i++) {
+            sum += evenfinder(i);
         }
         System.out.println("The sum is: " + sum);
+    }
+
+    static int evenfinder(int number) {
+        if (number % 2 == 0) {
+            return number;
+        } else {
+            return 0;
+        }
     }
 }
