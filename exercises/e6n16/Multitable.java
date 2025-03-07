3db5bdce5069e9f534ba89f5ced7f526323b5920
HelloWorld <business.reza.7757@gmail.com>
2025-03-07 18:53:03 +0330

    Exercise chapter6 number16

---
+++ exercises/e6n16/Multitable.java
@@ -0,0 +1,12 @@
+package exercises.e6n16;
+
+public class Multitable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
